b0fff2aefad859432049786fbd5611ee12184730
Seo Seongwon <ohzls@nate.com>
2025-04-15 16:39:39 +0900

    fix: CORS 설정 수정 - 모든 origin 허용

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ const CLOUD_RUN_TOKEN = process.env.CLOUD_RUN_TOKEN || 'MIIEvQIBADANBgkqhkiG9w0B
 // CORS 미들웨어 설정
 app.use((req, res, next) => {
   res.set({
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    'Access-Control-Allow-Credentials': 'true'
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
   });
 
   // OPTIONS 요청 처리
